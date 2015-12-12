c4f4584cb5e94dda185e5444a9e45e1d1a760962
unknown <iagoamaro@hotmail.com>
2015-12-12 00:53:03 -0200

    aaa

---
+++ list.py
@@ -0,0 +1,10 @@
+def exe(n):
+    
+
+        lista = []
+        for i in range(0, n + 1):
+             lista.append(i)
+             print(list(lista))
+             
+a= int(input("a: "))
+exe(a)
